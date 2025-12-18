b662c3e4946672b739430f46c05545aafad48be7
fantom <fantom@DESKTOP-K4T4BM2>
2025-12-18 20:29:43 +0200

    AppConfig -> connectionString

---
+++ DAL/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class AppConfig
+    {
+        public static string connectionString = "\"Server=.\\\\SQLEXPRESS;Database=EquipmentAccountingDB;Trusted_Connection=True;TrustServerCertificate=True\"";
+    }
+}
